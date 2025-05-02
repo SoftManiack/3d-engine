1778168c011112ba4af3695297fdb91f6789646f
Suckhodoev Artem <fetelupercal@gmail.com>
2025-05-02 17:34:44 +0500

    layout menu

--- engine.js
+++ engine.js
@@ -1,7 +1,7 @@
 export class Engine {
 
-    cursorX = 0.0
-    cursorY = 0.0
+    cursorX
+    cursorY
 
     vertexShaderSource = `
         attribute vec2 aPosition;
@@ -9,14 +9,17 @@ export class Engine {
             gl_Position = vec4(aPosition, 0.0, 1.0);
         }
     `;
-
+    
     fragmentShaderSource = `
         void main() {
             gl_FragColor = vec4(0.2, 0.6, 1.0, 1.0); // голубой цвет
         }
     `;
-
-    constructor() {}
+        
+    constructor() {
+        this.cursorX = 0.0
+        this.cursorY = 0.0
+    }
 
     createProgram(){
         
---
+++ init.js
@@ -0,0 +1,7 @@
+console.log("init")
+
+const canvas = document.getElementById('glcanvas');
+const gl = canvas.getContext('webgl');
+
+console.log("init")
+
